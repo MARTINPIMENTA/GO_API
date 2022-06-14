256bc8bc2170ebf8dd2beb0634d4fc91b7c34cbb
Martin Federico Pimenta Smurra <martin.pimenta@mercadolibre.com>
2022-06-14 15:44:42 -0300

    added articles functionality

--- src/api/domain/article/article.go
+++ src/api/domain/article/article.go
@@ -3,6 +3,7 @@ package article
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -17,21 +18,44 @@ type (
 	Articles []Article
 )
 
+var articles Articles
+
 // AllArticles returns the array of articles.
 func AllArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{
-			Title:   "Test Title",
-			Desc:    "Test Description",
-			Content: "Hello World",
-		},
+	// simulation to DB Get request
+	articlesResponse := articles
+
+	// Empty DB error case
+	if len(articlesResponse) == 0 {
+		fmt.Println("Endpoint Hit with error, no articles")
+		fmt.Fprintf(w, "No articles to return from DB")
+		return
 	}
 
+	// Success
 	fmt.Println("Endpoint Hit: All Articles Endpoint")
 	json.NewEncoder(w).Encode(articles)
 }
 
 // testPostArticles only returns a message, dummy func.
 func PostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This also works, POST method")
+	article := new(Article)
+
+	// Body error case
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error formatting request")
+		return
+	}
+
+	// Unmarshal body error case
+	err = json.Unmarshal(body, article)
+	if err != nil {
+		fmt.Fprintf(w, "Body unmarshall error")
+		return
+	}
+
+	// Success
+	articles = append(articles, *article)
+	fmt.Fprintf(w, "Article Posted!")
 }
