11ee7150a41cec9cbaffe2d7b26047c1a8931318
Martin Federico Pimenta Smurra <martin.pimenta@mercadolibre.com>
2022-06-14 14:16:51 -0300

    lambda function added for test purposes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/MARTINPIMENTA/pimen_rest_api_go
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/aws/aws-lambda-go v1.32.0
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -6,17 +6,43 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gorilla/mux"
 )
 
-type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+// Entities por lambda function Request and Response.
+type (
+	Request struct {
+		ID    float64 `json:"id"`
+		Value string  `json:"value"`
+	}
+
+	Response struct {
+		Message string `json:"message"`
+		Ok      bool   `json:"ok"`
+	}
+)
+
+// Handler for lambda function, we use this name to show the difference between handlers.
+func HandlerLambda(request Request) (Response, error) {
+	return Response{
+		Message: fmt.Sprintf("Process request ID %f", request.ID),
+		Ok:      true,
+	}, nil
 }
 
-type Articles []Article
+// Entities for article functionalities.
+type (
+	Article struct {
+		Title   string `json:"title"`
+		Desc    string `json:"desc"`
+		Content string `json:"content"`
+	}
 
+	Articles []Article
+)
+
+// allArticles returns the array of articles.
 func allArticles(w http.ResponseWriter, r *http.Request) {
 	articles := Articles{
 		Article{
@@ -30,24 +56,34 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+// testPostArticles only returns a message, dummy func.
 func testPostArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This also works, POST method")
 }
 
+// homePage func to test if the API is up and running.
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
+// handleRequests function to route endpoints to their handlers.
 func handleRequests() {
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
+	// home endpoint
 	myRouter.HandleFunc("/", homePage)
+	// Get endpoint for articles
 	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	// Post endpoint for articles
 	myRouter.HandleFunc("/articlespost", testPostArticles).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
+// main func.
 func main() {
 	handleRequests()
+
+	// Initialization for lambda functions.
+	lambda.Start(HandlerLambda)
 }
